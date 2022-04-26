45de7992233e07ea9be535ceb6dfb9437c713d2a
Junshan Liu <junshan_liu@esri.com>
2022-04-26 16:42:52 +0800

    sample 1

---
+++ dt/sample_iris.py
@@ -0,0 +1,11 @@
+from sklearn.datasets import load_iris
+from sklearn import tree
+import matplotlib.pyplot as plt
+
+iris = load_iris()
+X, y = iris.data, iris.target
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, y)
+
+tree.plot_tree(clf)
+plt.show()
